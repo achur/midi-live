8cd4ef4de0cbb7af62cfab4529348956cc867679
Alex Churchill <al.churchill@gmail.com>
2017-03-11 14:33:10 -0800

    Set up structure

---
+++ latest/midilive.js
@@ -0,0 +1,10 @@
+/**
+ * Entry point for midilive.js. Used to list controllers and attach them to
+ * instruments.
+ */
+class MIDILive {
+    init() {
+        console.log('ahoy');
+    }
+}
+export default MIDILive;
---
+++ src/midilive.ts
@@ -0,0 +1,13 @@
+
+
+/**
+ * Entry point for midilive.js. Used to list controllers and attach them to
+ * instruments.
+ */
+class MIDILive {
+  init() {
+    console.log('ahoy');
+  }
+}
+
+export default MIDILive;
